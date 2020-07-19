97dd21a6186c8a13a145d0dbb7524aab2ea250ab
study <lngthv00@gmail.com>
2020-07-20 01:13:47 +0700

    delete node_modules

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Ok');
+})
+
+app.listen(3000, function(req, res){
+    console.log('connect');
+})
