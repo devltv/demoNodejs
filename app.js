0c9556f2f8803ff9ca98bfda0de7917c67c0a848
study <lngthv00@gmail.com>
2020-07-20 01:27:52 +0700

    push about

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', function(req, res){
     res.send('Ok');
 })
+app.get('/about', function(req, res){
+    res.send('about ok');
+})
 
 app.listen(3000, function(req, res){
     console.log('connect');
