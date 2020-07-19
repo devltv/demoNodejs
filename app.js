6c6a7da4939156f8135e1ad2db877344e3427b57
study <lngthv00@gmail.com>
2020-07-20 01:34:19 +0700

    contact create

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.get('/', function(req, res){
 app.get('/about', function(req, res){
     res.send('about ok');
 })
+app.get('/contact', function(req, res){
+    res.send('contact ok');
+})
 
 app.listen(3000, function(req, res){
     console.log('connect');
